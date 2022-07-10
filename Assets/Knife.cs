70be2cf1a0947e62d7f47654ea63de77f9340a0a
hktrollgames <hktrollgames@gmail.com>
2022-07-10 14:24:01 +0900

    칼 위로 움직임

---
+++ Assets/Knife.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody2D;
+    public Vector2 force = new Vector2(0, 10f);
+    void Start()
+    {
+        print("Start");
+        rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+        rigidbody2D.AddForce(force, ForceMode2D.Impulse);
+    }
+}
